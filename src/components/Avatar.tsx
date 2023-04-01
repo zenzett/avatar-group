58c689c2acb233f4b1b91c6e38e9a049c4fd2bd2
Ahmad Zain AF <zainazharul@gmail.com>
2023-04-01 21:52:12 +0700

    project init

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+interface AvatarProps {
+  name: string;
+  picture?: string;
+  size: string;
+}
+
+export const Avatar: React.FC<AvatarProps> = ({ name, picture, size }) => {
+  const initial = name.charAt(0) + name.charAt(2);
+  const noPic = picture === "";
+  let borderSize = "";
+  let textSize = "";
+
+  if (size === "xs") {
+    borderSize = "h-8 w-8 border-2";
+    textSize = "text-[0.75rem]";
+  } else if (size === "sm") {
+    borderSize = "h-12 w-12 border-2";
+    textSize = "text-[1rem]";
+  } else if (size === "md") {
+    borderSize = "h-16 w-16 border-4";
+    textSize = "text-[1.5rem]";
+  } else if (size === "lg") {
+    borderSize = "h-20 w-20 border-4";
+    textSize = "text-[2rem]";
+  }
+
+  return (
+    <>
+      <div
+        className={`flex bg-gray-300 border-white rounded-full justify-center items-center ${borderSize}`}
+      >
+        {!noPic && (
+          <img src={picture} alt="" className="rounded-full h-full w-full" />
+        )}
+        {noPic && <span className={`uppercase ${textSize}`}>{initial}</span>}
+      </div>
+    </>
+  );
+};
+
+interface AvatarGroupProps {
+  users: Array<{ name: string; picture?: string }>;
+  maxLength: number;
+  size: string;
+}
+
+export const AvatarGroup: React.FC<AvatarGroupProps> = ({
+  users,
+  maxLength,
+  size,
+}) => {
+  const visibleUsers = users.slice(0, maxLength);
+  const excessUsers = users.length - maxLength;
+
+  let borderSize = "";
+  let textSize = "";
+
+  if (size === "xs") {
+    borderSize = "h-8 w-8 border-2";
+    textSize = "text-[0.75rem]";
+  } else if (size === "sm") {
+    borderSize = "h-12 w-12 border-2";
+    textSize = "text-[1rem]";
+  } else if (size === "md") {
+    borderSize = "h-16 w-16 border-4";
+    textSize = "text-[1.5rem]";
+  } else if (size === "lg") {
+    borderSize = "h-20 w-20 border-4";
+    textSize = "text-[2rem]";
+  }
+
+  return (
+    <div className="flex -space-x-4 hover:-space-x-1 hover:cursor-pointer">
+      {visibleUsers.map((user, index) => (
+        <div title={user.name} className="duration-300 active:scale-75">
+          <Avatar
+            key={index}
+            name={user.name}
+            picture={user.picture}
+            size={size}
+          />
+        </div>
+      ))}
+      {excessUsers > 0 && (
+        <div
+          title={`see ${excessUsers} more...`}
+          className={`flex bg-gray-300 border-white rounded-full justify-center items-center duration-300 active:scale-75 ${borderSize}`}
+        >
+          <span className={`uppercase ${textSize}`}>{`+${excessUsers}`}</span>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+
+import { AvatarGroup } from "../components/Avatar";
+
+const users = [
+  {
+    name: "Tomi Budi",
+    picture: "",
+  },
+  {
+    name: "Arlo Syahputra",
+    picture:
+      "https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png",
+  },
+  {
+    name: "Pablo Esmeralda",
+    picture: "",
+  },
+  {
+    name: "Bob Smith",
+    picture:
+      "https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png",
+  },
+  {
+    name: "Alice Johnson",
+    picture: "",
+  },
+  {
+    name: "Mika Tambayong",
+    picture:
+      "https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png",
+  },
+  {
+    name: "Carl Johnson",
+    picture:
+      "https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png",
+  },
+  {
+    name: "Tom Raider",
+    picture: "",
+  },
+  {
+    name: "Felix Martin",
+    picture: "",
+  },
+  {
+    name: "Steve Ausnih",
+    picture:
+      "https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png",
+  },
+  {
+    name: "Tomi Budi",
+    picture: "",
+  },
+  {
+    name: "Arlo Syahputra",
+    picture:
+      "https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png",
+  },
+  {
+    name: "Pablo Esmeralda",
+    picture: "",
+  },
+  {
+    name: "Bob Smith",
+    picture:
+      "https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png",
+  },
+  {
+    name: "Alice Johnson",
+    picture: "",
+  },
+  {
+    name: "Mika Tambayong",
+    picture:
+      "https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png",
+  },
+  {
+    name: "Carl Johnson",
+    picture:
+      "https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png",
+  },
+  {
+    name: "Tom Raider",
+    picture: "",
+  },
+  {
+    name: "Felix Martin",
+    picture: "",
+  },
+  {
+    name: "Steve Ausnih",
+    picture:
+      "https://w7.pngwing.com/pngs/980/886/png-transparent-male-portrait-avatar-computer-icons-icon-design-avatar-flat-face-icon-people-head-cartoon.png",
+  },
+];
+
+const index = () => {
+  return (
+    <div className="">
+      <AvatarGroup users={users} maxLength={14} size="md" />
+    </div>
+  );
+};
+
+export default index;
